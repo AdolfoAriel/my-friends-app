de3aa1d379ba5c3a9d85948b19e36d63a32d15b6
AdolfoAriel <arielnegre@gmail.com>
2023-11-23 13:44:21 -0600

    c

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import express from 'express';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,3 +16,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+const app = express()
+
+app.listen(3000, () => console.log("Api turning in the PORT 5001"))
+
+app.get('/', (req,res) => res.json("Mira!! ya logre subir algo a la nube, te amo Sofia <3"))
