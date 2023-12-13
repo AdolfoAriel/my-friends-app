cff253fa457b9beeb71e17f9b7df64e2bd268fbd
adolfoariel <arielnegre@gmail.com>
2023-12-12 22:05:59 -0600

    final

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import '../hojas-de-estilo/contacto.css'
 function Contacto() {
   return (
     <div className='contenedor-principal-contacto' style={{padding:'4cm'}}>
       
-      <h1>2231058415
+      <h1 className='numero'>2231058415
         <br/>
         adolfoarielnj@gmail.com
       </h1>
