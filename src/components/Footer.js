7d6d906c38e18f7c7fd4f6bac1ecb63ce6274e38
adolfoariel <arielnegre@gmail.com>
2023-12-15 22:16:55 -0600

    s

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div className='footer'>Citado APA: Del Moral, M. & Rodriguez, J. (s.f.). Ejemplo de Derechos De Autor.Ejemplo de. Recuperado el 31 de Enero de 2023 
     de https://www.ejemplode.com/68-derecho/2844-ejemplo_de_derechos_de_autor.html
     para mas informacion
-   
+   cambiar todo 
 
     Fuente: https://www.ejemplode.com/68-derecho/2844-ejemplo_de_derechos_de_autor.html#ixzz7vnU6iYWQ</div>
   )
